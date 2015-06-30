5b43452917838ad4fdbf63a4d765d3e387fd987e
Angel Lopez <alrabada@gmail.com>
2015-06-30 21:07:09 +0200

    random version 1

---
+++ random.js
@@ -0,0 +1,10 @@
+var numero = Math.random();
+
+if (numero <= 0.5){
+	console.log('\n' + numero + ' MENOR que 0,5 \n');
+}
+else{
+	console.log('\n' + numero + ' MAYOR que 0,5 \n');
+}
+
+console.log('CAMBIO PARA PROBAR UNA NUEVA VERSIÓN');
